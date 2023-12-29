8928c54521a5e13be3a100f1453eccb66fcd932d
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2023-12-30 01:07:06 +0300

    Update Program.cs

--- designPatterns/Singleton/Program.cs
+++ designPatterns/Singleton/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace Singleton
         static void Main(string[] args)
         {
             var customerManager = CustomerManager.CreateAsSingleton();
+            Console.ReadLine();
         }
     }
 
@@ -18,20 +19,28 @@ namespace Singleton
     {
         private static CustomerManager _customerManager;
         static object _lockObject=new object();
+
+        //sınıfın tek bir örneğinin olması için constructor,sınıfın dışarıdan new anahtar kelimesi ile oluşturulmasını engeller
         private CustomerManager()
         {
 
         }
 
+        //singleton örneğini oluşturan ve mevcut bir örneğin olup olmadığını kontrol eden metot
+        //lock sayesinde bu metot aynı anda sadece bir iş parçacığı tarafından çağrılabilir.
         public static CustomerManager CreateAsSingleton()
         {
+            //lock ile iş parçacığı güvenliği sağlanır
             lock (_lockObject)
             {
+                //_customerManager örneği null ise yeni örnek oluşturulur
                 if(_customerManager == null)
                 {
                     _customerManager = new CustomerManager();
+                    Console.WriteLine("Yeni örnek oluşturuldu");
                 }
             }
+            //kilit açılır ve oluşturulan veya mevcut olan _customerManager nesnesi geri döndürülür
             return _customerManager;
 
         }
