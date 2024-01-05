e261f71b56966da342a7019a39c02ebf970f57bc
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2024-01-05 13:03:11 +0300

    factory method added

---
+++ designPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory: ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new NdLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new SdLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class NdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NdLogger");
+        }
+    }
+    public class SdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with SdLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
