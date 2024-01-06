f140d7415d7db1e119c6efa8b80fd7f07d65ae08
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2024-01-06 22:44:52 +0300

    strategy added

---
+++ designPatterns/Strategy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.calculatorBase= new Before2010CreditCalculatorBase();
+            customerManager.SaveCredit();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculatorBase : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before 2010");
+        }
+    }
+
+    class After2010CreditCalculatorBase : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase calculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            calculatorBase.Calculate();
+        }
+    }
+
+
+
+
+}
